9a3950ad6d05ea27f0323c78d4a1cc6f1dc09bd1
grace-kerr <gker710@aucklanduni.ac.nz>
2023-09-03 08:14:03 +1000

    :sparkles: add title page to game
    Additionally I deleted all unnecessary room.fxl components

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -43,11 +43,10 @@ public class App extends Application {
    */
   @Override
   public void start(final Stage stage) throws IOException {
-    Parent root = loadFxml("room");
+    Parent root = loadFxml("title");
     scene = new Scene(root, 600, 470);
     stage.setScene(scene);
     stage.show();
     root.requestFocus();
   }
-
 }
---
+++ src/main/java/nz/ac/auckland/se206/controllers/TitleController.java
@@ -0,0 +1,22 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import nz.ac.auckland.se206.App;
+
+public class TitleController {
+
+  @FXML Pane fullScreenPane;
+
+  /** Initializes the title view */
+  public void initialize() {
+    // Initialization code goes here
+  }
+
+  @FXML
+  public void onMouseClicked(MouseEvent event) throws IOException {
+    App.setRoot("room");
+  }
+}
