d8474b5f5736952c7f06300f79e1fc38531cbb52
rtal831 <rtal831@aucklanduni.ac.nz>
2023-09-05 21:12:30 +1200

    :bricks: implement zoomandpanhandler class to localise all zooming and panning logic

--- src/main/java/nz/ac/auckland/se206/controllers/ExpositionController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ExpositionController.java
@@ -1,8 +1,5 @@
 package nz.ac.auckland.se206.controllers;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
@@ -10,103 +7,36 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.transform.Scale;
-import javafx.util.Duration;
 
-/** Controller class for the room view. */
 public class ExpositionController implements Controller {
 
-  @FXML private Pane panSpaceship;
-  @FXML private Group panzoomgroup;
-  @FXML private Rectangle recTest;
-  @FXML private Button btnSettings;
+    @FXML private Pane panSpaceship;
+    @FXML private Group panzoomgroup;
+    @FXML private Rectangle recTest;
+    @FXML private Button btnSettings;
 
-  private double pressedX, pressedY;
-  private double deltaX, deltaY;
-  private double zoomFactor = 1.25;
-  private double minScaleFactor = 0.9;
-  private double maxScaleFactor = 2.0;
-  private Scale scale = new Scale();
+    private ZoomAndPanHandler zoomAndPanHandler;
 
-  public void initialize() {
-    // Add zoom functionality to panzoomgroup
-    panzoomgroup.getTransforms().add(scale);
-  }
+    public void initialize() {
+        zoomAndPanHandler = new ZoomAndPanHandler(panzoomgroup);
+    }
 
-  @FXML
-  private void onPress(MouseEvent event) {
-    // Store the initial position when the mouse is pressed
-    pressedX = event.getSceneX();
-    pressedY = event.getSceneY();
-    System.out.println("Mouse pressed at " + pressedX + ", " + pressedY);
-  }
+    @FXML
+    private void onPress(MouseEvent event) {
+        zoomAndPanHandler.onPress(event);
+    }
 
-  @FXML
-  private void onDrag(MouseEvent event) {
-    deltaX = event.getSceneX() - pressedX;
-    deltaY = event.getSceneY() - pressedY;
+    @FXML
+    private void onDrag(MouseEvent event) {
+        zoomAndPanHandler.onDrag(event);
+    }
 
-    // Calculate the new position after panning
-    double newX = panzoomgroup.getTranslateX() + deltaX;
-    double newY = panzoomgroup.getTranslateY() + deltaY;
+    @FXML
+    private void onScroll(ScrollEvent event) {
+        zoomAndPanHandler.onScroll(event);
+    }
 
-    // Define the boundaries for panning (adjust these values as needed)
-    // double minX = -100 * scaleFactor;
-    // double minY = -100 * scaleFactor;
-    // double maxX = 100 * scaleFactor;
-    // double maxY = 100 * scaleFactor;
-
-    // newX = Math.min(Math.max(newX, minX), maxX);
-    // newY = Math.min(Math.max(newY, minY), maxY);
-
-    // Apply the translation without restricting boundaries
-    panzoomgroup.setTranslateX(newX);
-    panzoomgroup.setTranslateY(newY);
-
-    pressedX = event.getSceneX();
-    pressedY = event.getSceneY();
-    System.out.println("Mouse dragged to " + pressedX + ", " + pressedY);
-  }
-
-  @FXML
-  private void onScroll(ScrollEvent event) {
-
-    // Zoom in or out
-    double scaleFactor = (event.getDeltaY() < 0) ? 1 / zoomFactor : zoomFactor;
-
-    double currentScaleFactor = scale.getX();
-    double newScaleFactor = currentScaleFactor * scaleFactor;
-    newScaleFactor = clamp(newScaleFactor, minScaleFactor, maxScaleFactor);
-    double pivotX = event.getX();
-    double pivotY = event.getY();
-    double scaleChange = newScaleFactor / currentScaleFactor - 1;
-
-    // Calculate the translation adjustments
-    double translateX =
-        panzoomgroup.getTranslateX()
-            - scaleChange * (pivotX - panzoomgroup.getBoundsInParent().getMinX());
-    double translateY =
-        panzoomgroup.getTranslateY()
-            - scaleChange * (pivotY - panzoomgroup.getBoundsInParent().getMinY());
-
-    // Smoothly animate the zoom
-    Timeline timeline =
-        new Timeline(
-            new KeyFrame(
-                Duration.millis(50),
-                new KeyValue(scale.xProperty(), newScaleFactor),
-                new KeyValue(scale.yProperty(), newScaleFactor),
-                new KeyValue(panzoomgroup.translateXProperty(), translateX),
-                new KeyValue(panzoomgroup.translateYProperty(), translateY)));
-
-    timeline.play();
-
-    event.consume();
-  }
-
-  private double clamp(double value, double min, double max) {
-    return Math.min(Math.max(value, min), max);
-  }
+    
 
   @FXML
   private void settingsClicked() {
---
+++ src/main/java/nz/ac/auckland/se206/controllers/ZoomAndPanHandler.java
@@ -0,0 +1,93 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.transform.Scale;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
+public class ZoomAndPanHandler {
+
+    private final Group panzoomgroup;
+    private final Scale scale;
+
+    private double pressedX, pressedY;
+    private double deltaX, deltaY;
+    private double zoomFactor = 1.25;
+    private double minScaleFactor = 0.9;
+    private double maxScaleFactor = 2.0;
+    private double scaleFactor = 1.0;
+
+    public ZoomAndPanHandler(Group panzoomgroup) {
+        this.panzoomgroup = panzoomgroup;
+        this.scale = new Scale();
+        this.scale.setPivotX(0);
+        this.scale.setPivotY(0);
+        panzoomgroup.getTransforms().add(scale);
+    }
+
+    public void onPress(MouseEvent event) {
+        // Record the current mouse X and Y position on Node
+        pressedX = event.getSceneX();
+        pressedY = event.getSceneY();
+        System.out.println("Mouse pressed at " + pressedX + ", " + pressedY);
+    }
+
+    public void onDrag(MouseEvent event) {
+        deltaX = event.getSceneX() - pressedX;
+        deltaY = event.getSceneY() - pressedY;
+
+        // Calculate the new position after panning
+        double newX = panzoomgroup.getTranslateX() + deltaX;
+        double newY = panzoomgroup.getTranslateY() + deltaY;
+
+        // Apply the translation without restricting boundaries
+        panzoomgroup.setTranslateX(newX);
+        panzoomgroup.setTranslateY(newY);
+
+        pressedX = event.getSceneX();
+        pressedY = event.getSceneY();
+        System.out.println("Mouse dragged to " + pressedX + ", " + pressedY);
+    }
+
+    public void onScroll(ScrollEvent event) {
+        
+        scaleFactor = (event.getDeltaY() < 0) ? 1 / zoomFactor : zoomFactor;
+        double currentScaleFactor = scale.getX();
+        double newScaleFactor = currentScaleFactor * scaleFactor;
+        scaleFactor = newScaleFactor;
+        newScaleFactor = clamp(newScaleFactor, minScaleFactor, maxScaleFactor);
+        double pivotX = event.getX();
+        double pivotY = event.getY();
+        double scaleChange = newScaleFactor / currentScaleFactor - 1;
+
+        // Calculate the translation adjustments
+        double translateX =
+                panzoomgroup.getTranslateX()
+                        - scaleChange * (pivotX - panzoomgroup.getBoundsInParent().getMinX());
+        double translateY =
+                panzoomgroup.getTranslateY()
+                        - scaleChange * (pivotY - panzoomgroup.getBoundsInParent().getMinY());
+
+        // Smoothly animate the zoom
+        Timeline timeline =
+            new Timeline(
+                new KeyFrame(
+                    Duration.millis(50),
+                    new KeyValue(scale.xProperty(), newScaleFactor),
+                    new KeyValue(scale.yProperty(), newScaleFactor),
+                    new KeyValue(panzoomgroup.translateXProperty(), translateX),
+                    new KeyValue(panzoomgroup.translateYProperty(), translateY)));
+
+        timeline.play();
+
+        event.consume();
+    };
+
+    private double clamp(double value, double min, double max) {
+        return Math.min(Math.max(value, min), max);
+    }
+}
