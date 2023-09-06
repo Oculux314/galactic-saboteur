ad735492b2d2960c4c89421ff1bb3b8e70b5f31f
Oculux314 <nate.williamson.nz@gmail.com>
2023-09-06 22:24:35 +1200

    :memo: add javadoc to App class

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -13,28 +13,43 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import nz.ac.auckland.se206.controllers.MainController;
 
-/**
- * This is the entry point of the JavaFX application, while you can change this class, it should
- * remain as the class that runs the JavaFX application.
- */
+/** The entry point of the JavaFX application, representing the top-level application. */
 public class App extends Application {
   private static Stage stage;
   private static Map<Root.Name, Root> roots = new HashMap<>(); // Stores all FXML/Controller pairs
 
+  /**
+   * The entry point of the application.
+   *
+   * @param args Command line arguments.
+   */
   public static void main(final String[] args) {
     launch();
   }
 
-  public static Root getRoot(final Root.Name rootName) throws IOException {
+  /**
+   * Returns the root with the given name, or null if it does not exist.
+   *
+   * @param rootName The name of the root to get.
+   * @return The root with the given name, or null if it does not exist.
+   */
+  public static Root getRoot(final Root.Name rootName) {
     return roots.get(rootName); // Null if does not exist
   }
 
+  /**
+   * Sets the root with the given name as the child of the main pane. Creates the root if it does
+   * not exist.
+   *
+   * @param rootName The name of the root to set.
+   * @throws IOException If the root does not exist and the FXML file for the root is not found.
+   */
   public static void setRoot(final Root.Name rootName) throws IOException {
     if (!roots.containsKey(rootName)) {
       makeRoot(rootName); // Automatically make root if does not exist
     }
 
-    Parent fxml = roots.get(rootName).getFxml();
+    Parent fxml = getRoot(rootName).getFxml();
     Pane panMain = ((MainController) roots.get(Root.Name.MAIN).getController()).getMainPane();
 
     // Set root as child of main pane
@@ -44,25 +59,49 @@ public class App extends Application {
     fxml.requestFocus();
   }
 
+  /**
+   * Creates a root with the given name, and stores it in the roots map.
+   *
+   * @param rootName The name of the root to create.
+   * @throws IOException If the FXML file for the root is not found.
+   */
   private static void makeRoot(final Root.Name rootName) throws IOException {
     String fxml = rootName.toString().toLowerCase();
     FXMLLoader loader = new FXMLLoader(App.class.getResource("/fxml/" + fxml + ".fxml"));
     roots.put(rootName, new Root(loader));
   }
 
+  /**
+   * Returns the primary stage of the application.
+   *
+   * @return The primary stage of the application.
+   */
   public static Stage getStage() {
     return stage;
   }
 
+  /**
+   * Returns the scene of the primary stage of the application.
+   *
+   * @return The scene of the primary stage of the application.
+   */
   public static Scene getScene() {
     return stage.getScene();
   }
 
   /**
-   * This method is invoked when the application starts. It loads the stage and scene, as well as
-   * the main and title roots.
+   * This method is invoked when the application starts. It performs all neccessary setup,
+   * including:
    *
-   * @param stage The primary stage of the application.
+   * <ul>
+   *   <li>Initialise stage and scene
+   *   <li>Set up root graph
+   *   <li>Link style sheet and listeners
+   *   <li>Set stage properties
+   *   <li>Show stage
+   * </ul>
+   *
+   * @param newStage The primary stage of the application.
    * @throws IOException If either "main.fxml" or "title.fxml" is not found in
    *     "src/main/resources/fxml/".
    */
@@ -82,7 +121,7 @@ public class App extends Application {
     // Stylesheet
     scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
 
-    // Scene size listeners
+    // Listeners
     ((MainController) roots.get(Root.Name.MAIN).getController()).addSceneListeners();
 
     // Properties
