4284cd824f525545909d8c4af37dcb58a828008a
Oculux314 <nate.williamson.nz@gmail.com>
2023-09-10 18:16:07 +1200

    :lipstick: add temporary buttons to title screen
    SceneBuilder isn't recognising my custom button component, so I've added
    them programmatically via the TitleController class until Nasser
    responds on Ed.
    
    Added placeholder assets.
    
    Wrapped fullscreen pane in container pane so
    clicking a button doesn't trigger the next screen.

--- src/main/java/nz/ac/auckland/se206/controllers/TitleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/TitleController.java
@@ -6,15 +6,31 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.Screen;
+import nz.ac.auckland.se206.components.IconButton;
 
 /** Controller class for the title screen. */
 public class TitleController implements Controller {
 
-  /** Pane that takes up the entire screen. */
+  @FXML Pane panContainer;
   @FXML Pane panFullScreen;
 
+  @FXML
+  private void initialize() {
+
+    IconButton btnSettings = new IconButton("/images/settings.png");
+    btnSettings.setLayoutX(300);
+    btnSettings.setLayoutY(420);
+    panContainer.getChildren().add(btnSettings);
+
+    IconButton btnPlay = new IconButton("/images/play.png");
+    btnPlay.setLayoutX(420);
+    btnPlay.setLayoutY(420);
+    panContainer.getChildren().add(btnPlay);
+  }
+
   /**
-   * Called when the mouse is clicked on the full screen pane. Moves control to the exposition screen.
+   * Called when the mouse is clicked on the full screen pane. Moves control to the exposition
+   * screen.
    *
    * @param event The mouse event.
    * @throws IOException If the exposition.fxml file is not found.
