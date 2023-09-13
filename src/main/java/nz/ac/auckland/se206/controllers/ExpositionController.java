4a4e68f6683fb00bb4c8d6cf51790d3bbfb56071
rtal831 <rtal831@aucklanduni.ac.nz>
2023-09-13 18:18:43 +1200

    :sparkles: Implemented slideshow with four images

--- src/main/java/nz/ac/auckland/se206/controllers/ExpositionController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ExpositionController.java
@@ -1,9 +1,14 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.Screen;
 
@@ -12,6 +17,22 @@ public class ExpositionController implements Controller {
 
   /** Pane that takes up the entire screen. */
   @FXML Pane panFullScreen;
+
+  @FXML ImageView imageView = new ImageView();
+  private int currentImageIndex = 0;
+
+  private String[] imagePaths = {
+    "/images/expo2.jpg",
+    "/images/expo3.jpg",
+    "/images/expo4.jpg"
+  };
+
+  @FXML
+  public void initialize() {
+    Image image = new Image("/images/expo1.jpg");
+    imageView.setImage(image);
+    startSlideshow();
+  }
 
   /**
    * Called when the mouse is clicked on the full screen pane. Moves control to the game screen.
@@ -22,5 +43,22 @@ public class ExpositionController implements Controller {
   @FXML
   public void onMouseClicked(MouseEvent event) throws IOException {
     App.setScreen(Screen.Name.GAME);
+  }
+
+  private void startSlideshow() {
+    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1.5), event -> showNextImage()));
+    timeline.setCycleCount(Timeline.INDEFINITE);
+    timeline.play();
+  }
+
+  private void showNextImage() {
+    if (currentImageIndex < imagePaths.length) {
+      String imagePath = imagePaths[currentImageIndex];
+      Image image = new Image(getClass().getResourceAsStream(imagePath));
+      imageView.setImage(image);
+      currentImageIndex++;
+    } else {
+      imageView.setImage(null);
+    }
   }
 }
