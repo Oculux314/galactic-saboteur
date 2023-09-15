3a71cd3e0be37d6ea4a8a15047a675e02fac604b
grace-kerr <gker710@aucklanduni.ac.nz>
2023-09-15 22:49:35 +1200

    :sparkles: add clue information box Clue information will hear when the user hovers over clue icon.

--- src/main/java/nz/ac/auckland/se206/controllers/GameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GameController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
@@ -30,6 +31,8 @@ public class GameController implements Controller {
   @FXML private Polyline btnPanelHide;
   @FXML private Polyline btnPanelShow;
   @FXML private Group panelContainer;
+  @FXML private Rectangle clueInformationRectangle;
+  @FXML private Label lblClueInformation;
 
   private ZoomAndPanHandler zoomAndPanHandler;
 
@@ -89,5 +92,41 @@ public class GameController implements Controller {
   private void btnPanelHidePressed() {
     panelContainer.setLayoutX(-172);
     btnPanelHide.setVisible(false);
+  }
+
+  @FXML
+  private void clue1InformationShow(MouseEvent event) {
+    clueInformationRectangle.setOpacity(1);
+    lblClueInformation.setText("Clue 1");
+  }
+
+  @FXML
+  private void clue1InformationHide(MouseEvent event) {
+    clueInformationRectangle.setOpacity(0);
+    lblClueInformation.setText("");
+  }
+
+  @FXML
+  private void clue2InformationShow(MouseEvent event) {
+    clueInformationRectangle.setOpacity(1);
+    lblClueInformation.setText("Clue 2");
+  }
+
+  @FXML
+  private void clue2InformationHide(MouseEvent event) {
+    clueInformationRectangle.setOpacity(0);
+    lblClueInformation.setText("");
+  }
+
+  @FXML
+  private void clue3InformationShow(MouseEvent event) {
+    clueInformationRectangle.setOpacity(1);
+    lblClueInformation.setText("Clue 3");
+  }
+
+  @FXML
+  private void clue3InformationHide(MouseEvent event) {
+    clueInformationRectangle.setOpacity(0);
+    lblClueInformation.setText("");
   }
 }
