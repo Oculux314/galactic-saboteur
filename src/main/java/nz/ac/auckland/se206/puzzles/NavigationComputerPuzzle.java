5701f76677384e9953b1b711693e66ac9b6467f9
Oculux314 <nate.williamson.nz@gmail.com>
2023-09-20 20:09:07 +1200

    :bricks: implement tileType 'map' in computer puzzle
    Records what type each tile is in 2D array.

--- src/main/java/nz/ac/auckland/se206/puzzles/NavigationComputerPuzzle.java
+++ src/main/java/nz/ac/auckland/se206/puzzles/NavigationComputerPuzzle.java
@@ -7,28 +7,49 @@ import nz.ac.auckland.se206.components.ComputerTile.Connector;
 
 public class NavigationComputerPuzzle extends Puzzle {
 
+  private static final int NUM_ROWS = 3;
+  private static final int NUM_COLS = 4;
+
   @FXML private Group grpTiles;
-  ComputerTile[][] tiles = new ComputerTile[3][4];
+  private ComputerTile[][] tiles;
+  private ComputerTile.Connector[][] tileTypes;
 
   public void initialize() {
-    for (int i = 0; i < 12; i++) {
+    tiles = new ComputerTile[NUM_ROWS][NUM_COLS];
+    tileTypes = new ComputerTile.Connector[NUM_ROWS][NUM_COLS];
+
+    randomizeTileTypes();
+    chooseSolution();
+
+    for (int i = 0; i < (NUM_ROWS * NUM_COLS); i++) {
       makeTile(i);
     }
   }
 
+  private void randomizeTileTypes() {
+    for (int i = 0; i < NUM_ROWS; i++) {
+      for (int j = 0; j < NUM_COLS; j++) {
+        tileTypes[i][j] = Connector.RANDOM;
+      }
+    }
+  }
+
+  private void chooseSolution() {
+    // TODO: make randomised choose solution
+  }
+
   private void makeTile(int n) {
+    int row = n / NUM_COLS;
+    int col = n % NUM_COLS;
+
     ComputerTile tile = new ComputerTile();
     grpTiles.getChildren().add(tile);
-    tile.setType(Connector.RANDOM);
+    tile.setType(tileTypes[row][col]);
+    tiles[row][col] = tile;
 
-    // Dimensions
+    // Dimensions & position
     tile.setFitWidth(60);
     tile.setFitHeight(60);
-
-    // Layout
-    int row = n / 4;
-    int col = n % 4;
-    tiles[row][col] = tile;
     tile.setLayoutX(60 * col);
     tile.setLayoutY(60 * row);
   }
