060ac9728163e6d7041630d56991d5ff268a934b
Oculux314 <nate.williamson.nz@gmail.com>
2023-10-05 21:48:04 +1300

    Revert ":bricks: extract AnimatedButton.getHoverImage method as Utils.getImageWithSuffix"
    This reverts commit 00cf9be76488b98d3b38d17736030b40b2ab798c.

--- src/main/java/nz/ac/auckland/se206/Utils.java
+++ src/main/java/nz/ac/auckland/se206/Utils.java
@@ -1,22 +1,8 @@
 package nz.ac.auckland.se206;
 
-import javafx.scene.image.Image;
-
 public class Utils {
-
   public static String appendBeforeExtension(String url, String append) {
     int lastDot = url.lastIndexOf(".");
     return url.substring(0, lastDot) + append + url.substring(lastDot);
-  }
-
-  public static Image getImageWithSuffix(Image base, String suffix) {
-    String url = appendBeforeExtension(base.getUrl(), suffix);
-    Image image = new Image(url);
-
-    if (image.isError()) {
-      return base; // Default to normal image if image with suffix not found
-    }
-
-    return image;
   }
 }
