0d729df823ccdaea8ca3ef117c892d97cd9c92bc
rtal831 <rtal831@aucklanduni.ac.nz>
2023-10-17 00:56:05 +1300

    :art: Essential full stops for javadoc

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -227,7 +227,7 @@ public class App extends Application {
     threads.add(thread);
   }
 
-  /** Takes the single tts instance and stops it temporarily */
+  /** Takes the single tts instance and stops it temporarily. */
   public static void stopTts() {
     NotificationpanelController notificationpanelController =
         ((NotificationpanelController)
